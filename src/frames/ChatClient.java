7ddb9d8e5d0f94cb56914ddc2f1513c99721007f
aeloidii <aeloidii@gmail.com>
2023-12-26 20:44:44 +0100

    send file

--- src/frames/ChatClient.java
+++ src/frames/ChatClient.java
@@ -4,14 +4,17 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.util.Scanner;
 
 public class ChatClient extends JFrame {
     private static final String SERVER_IP = "127.0.0.1";
     private static final int SERVER_PORT = 8888;
+    private static DataOutputStream dataOutputStream = null;
 
     private Socket socket;
     private PrintWriter outputStream;
@@ -20,17 +23,24 @@ public class ChatClient extends JFrame {
     private JTextArea chatArea;
     private JTextField messageField;
     private String nickname;
+    private JButton sendFileButton;
 
     public ChatClient() {
+        // Prompt user for a nickname
+        while (true) {
+            this.nickname = JOptionPane.showInputDialog(this, "Enter your nickname:");
+            if (isValidNickname(nickname)) {
+                break;
+            } else {
+                JOptionPane.showMessageDialog(this, "Invalid nickname. Please choose a different one.");
+            }
+        }
+
         setTitle("Chat Client");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(400, 300);
         setLocationRelativeTo(null);
         setResizable(false);
-        
-
-        // Prompt user for a nickname
-        this.nickname = JOptionPane.showInputDialog(this, "Enter your nickname:");
 
         chatArea = new JTextArea();
         chatArea.setEditable(false);
@@ -46,14 +56,31 @@ public class ChatClient extends JFrame {
             }
         });
 
+        sendFileButton = new JButton("Send File");
+        sendFileButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                chooseAndSendFile();
+            }
+        });
+
         JPanel inputPanel = new JPanel(new BorderLayout());
         inputPanel.add(messageField, BorderLayout.CENTER);
         inputPanel.add(sendButton, BorderLayout.EAST);
         add(inputPanel, BorderLayout.SOUTH);
 
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.add(sendFileButton);
+
+        add(buttonPanel, BorderLayout.NORTH);
+
         connectToServer();
 
         setVisible(true);
+    }
+
+    private boolean isValidNickname(String nickname) {
+        return !nickname.trim().isEmpty();
     }
 
     private void connectToServer() {
@@ -62,15 +89,19 @@ public class ChatClient extends JFrame {
             outputStream = new PrintWriter(socket.getOutputStream(), true);
             inputStream = new Scanner(socket.getInputStream());
             
+            dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+
             // Send the chosen nickname to the server
             outputStream.println(nickname);
 
+            // Start the thread to listen for incoming messages
             new Thread(new Runnable() {
                 @Override
                 public void run() {
                     while (inputStream.hasNextLine()) {
                         String message = inputStream.nextLine();
-                        chatArea.append(message + "\n");
+                        SwingUtilities.invokeLater(() -> chatArea.append(message + "\n"));
                     }
                 }
             }).start();
@@ -81,9 +112,47 @@ public class ChatClient extends JFrame {
 
     private void sendMessage() {
         String message = messageField.getText();
-        outputStream.println(message);
+        outputStream.println(nickname + ": " + message);
         messageField.setText("");
     }
+
+    private void chooseAndSendFile() {
+        JFileChooser fileChooser = new JFileChooser();
+        int result = fileChooser.showOpenDialog(this);
+
+        if (result == JFileChooser.APPROVE_OPTION) {
+            File selectedFile = fileChooser.getSelectedFile();
+            sendFile(selectedFile);
+        }
+    }
+
+    private void sendFile(File file) {            
+    		outputStream.println(file.getName());
+            
+            outputStream.println("/file");
+            outputStream.println(file.getName());
+            int bytes = 0;
+            System.out.println(file);
+            try {
+	            FileInputStream fileInputStream = new FileInputStream(file);
+	            
+	            // send file size
+	            dataOutputStream.writeLong(file.length());
+	            System.out.println("size to create : "+ file.length());
+	            // break file into chunks
+	            byte[] buffer = new byte[4*1024];
+	            while ((bytes=fileInputStream.read(buffer))!=-1){
+	                dataOutputStream.write(buffer,0,bytes);
+	                dataOutputStream.flush();
+	            }
+	            System.out.println("the file has been sent.");
+	            fileInputStream.close();
+            }catch(Exception e) {
+            	e.printStackTrace();
+            }
+    }
+
+    
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
--- src/frames/ClientHandler.java
+++ src/frames/ClientHandler.java
@@ -1,15 +1,23 @@
 package frames;
 
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 import java.util.List;
 import java.util.Scanner;
 
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+
+
 public class ClientHandler implements Runnable {
+    private static final String FILE_SIGNAL = "/file";
+    
+    private static DataInputStream dataInputStream = null;
+
     private Socket clientSocket;
     private List<ClientHandler> clients;
     private PrintWriter outputStream;
+    private Scanner inputStream;
     private String nickname;
 
     public ClientHandler(Socket socket, List<ClientHandler> clients, String nickname) {
@@ -18,6 +26,8 @@ public class ClientHandler implements Runnable {
 
         try {
             this.outputStream = new PrintWriter(socket.getOutputStream(), true);
+            this.inputStream = new Scanner(socket.getInputStream());
+            ClientHandler.dataInputStream = new DataInputStream(socket.getInputStream());
             this.nickname = nickname;
         } catch (IOException e) {
             e.printStackTrace();
@@ -27,14 +37,30 @@ public class ClientHandler implements Runnable {
     @Override
     public void run() {
         try {
-            Scanner inputStream = new Scanner(clientSocket.getInputStream());
-
             while (inputStream.hasNextLine()) {
                 String message = inputStream.nextLine();
-                broadcast(nickname + ": " + message);
+
+                if (message.equals(FILE_SIGNAL)) {
+                    try {
+                        String nameOfFile = inputStream.nextLine();
+                        System.out.println("Creating the file !!");
+                        System.out.println(nameOfFile);
+
+                        // Ask the user for the destination directory using JavaFX DirectoryChooser
+                        String destinationDirectory = chooseDirectory();
+
+                        if (destinationDirectory != null) {
+                            receiveFile(nameOfFile, destinationDirectory);
+                        } else {
+                            System.out.println("File transfer cancelled by user.");
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    broadcast(nickname + ": " + message);
+                }
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         } finally {
             try {
                 clientSocket.close();
@@ -46,7 +72,40 @@ public class ClientHandler implements Runnable {
         }
     }
 
-    private void broadcast(String message) {
+    private String chooseDirectory() {
+        // Create a file chooser
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        fileChooser.setDialogTitle("Choose Destination Directory");
+
+        // Show the file chooser dialog
+        int result = fileChooser.showOpenDialog(null);
+
+        if (result == JFileChooser.APPROVE_OPTION) {
+            File selectedDirectory = fileChooser.getSelectedFile();
+            return selectedDirectory.getAbsolutePath();
+        } else {
+            return null; // User canceled the operation
+        }
+    }
+
+    private static void receiveFile(String fileName, String destinationDirectory) throws Exception{
+        int bytes = 0;
+        FileOutputStream fileOutputStream = new FileOutputStream(destinationDirectory + File.separator + fileName);
+        
+        long size = dataInputStream.readLong();    // read file size
+        System.out.println("size to create 1 : "+ size);
+        byte[] buffer = new byte[4*1024];
+        while (size > 0 && (bytes = dataInputStream.read(buffer, 0, (int)Math.min(buffer.length, size))) != -1) {
+            fileOutputStream.write(buffer,0,bytes);
+            size -= bytes;      // read upto file size
+        }
+        System.out.println("The file has been created in " + destinationDirectory);
+        fileOutputStream.close();
+    }
+
+
+    private synchronized void broadcast(String message) {
         for (ClientHandler client : clients) {
             client.sendMessage(message);
         }
